49a6b3a75beb4f8f15d7891e7b3508d9fbedf28e
khk <jasun1208@naver.com>
2021-06-14 00:45:40 +0900

    문제 설명 수정

--- algo2.py
+++ algo2.py
@@ -29,21 +29,21 @@ def solution(progresses, speeds):
 
 
 '''
-2) 첫번째가 100이 될때까지 loop 를 돌며 time 을 늘린다. 
+1) 첫번째가 100이 될때까지 loop 를 돌며 time 을 늘린다. 
 
     else --> time+=1
 
-3) (time =7) 이 되면  첫번째 값이93인놈이 100이 되어 if에 따라 pop 되고 count +=1
+2) (time =7) 이 되면  첫번째 값이93인놈이 100이 되어 if에 따라 pop 되고 count +=1
 
-4) 현재 time 이 7이기 때문에 두번째 값(30)도 if에 따라 pop 되고 count +=1 
-5) 세번째 값은 100이 안되기 때문에 loop를 돌며 time 을 늘리는데 
+3) 현재 time 이 7이기 때문에 두번째 값(30)도 if에 따라 pop 되고 count +=1 
+4) 세번째 값은 100이 안되기 때문에 loop를 돌며 time 을 늘리는데 
     2번과 달리 그전에 완성된 친구들 count 값이 있기 때문에 이 친구들을 출시해줘야함 
     따라서 answer 리스트에 append하고 count 초기화!!! 
     그후에 loop를 돌며 time 을 늘림
     여기까지 answer의 리스트 값은 [2] 이상태
 
 
-6) 세번째 값(55)이 100을 넘으면 count +=1 하고 
+5) 세번째 값(55)이 100을 넘으면 count +=1 하고 
     이 count 를 다시한번 answer 리스트에 append 해줌으로써 마지막 제품까지 출시 !
     마지막 count 1 을 answer 리스트에 넣으면 answer = [2, 1]!!!!!
 '''
